009cedddbd14861364ae481e7f233046319b6b33
ximik28 <ximik28@tut.by>
2022-12-15 21:14:46 +0300

    Выполнен Task1

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int toadEyesCount = 0;          //Жабьи глаза
-        int ghoulTearsCount = 0;        //Слезы вурдалака
-        int ravenBonesCount = 0;        //Кости ворона
-        int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        int toadEyesCount;          //Жабьи глаза
+        int ghoulTearsCount;        //Слезы вурдалака
+        int ravenBonesCount;        //Кости ворона
+        int dumplingsCount;         //Пельмени(а почему, собственно нет? xD )
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("Для приготовления эликсиров необходимо ввести количество ингредиентов (целые положительные числа)");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите количество жабьих глаз");
+        toadEyesCount = scan.nextInt();
+        System.out.println("Введите количество слез вурдалака");
+        ghoulTearsCount = scan.nextInt();
+        System.out.println("Введите количество костей ворона");
+        ravenBonesCount = scan.nextInt();
+        System.out.println("Введите количество пельменей");
+        dumplingsCount = scan.nextInt();
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -14,5 +26,42 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+
+        boolean isVigilance = false;
+        boolean isStrong = false;
+        boolean isSecrecy = false;
+        boolean isForbidden = false;
+
+        if (toadEyesCount >= 3 && ravenBonesCount >= 1) {
+            System.out.println("Вы можете приготовить эликсир зоркости.");
+            isVigilance = true;
+        } else {
+            System.out.println("На эликсир зоркости не хватает ингредиентов!");
+        }
+
+        if (ravenBonesCount >= 2 && dumplingsCount >= 4 && toadEyesCount >= 2) {
+            System.out.println("Вы можете приготовить эликсир стойкости.");
+            isStrong = true;
+        } else {
+            System.out.println("На эликсир стойкости не хватает ингредиентов!");
+        }
+
+        if (ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2) {
+            System.out.println("Вы можете приготовить эликсир скрытности.");
+            isSecrecy = true;
+        } else {
+            System.out.println("На эликсир скрытности не хватает ингредиентов!");
+        }
+
+        if (ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 4 && ravenBonesCount >= 3) {
+            System.out.println("Вы можете приготовить Запретный эликсир.");
+            isForbidden = true;
+        } else {
+            System.out.println("На Запретный эликсир не хватает ингредиентов!");
+        }
+
+        if (!isVigilance && !isStrong && !isSecrecy && !isForbidden) {
+            System.out.println("Не хватает ингредиентов для любого эликсира!");
+        }
     }
 }

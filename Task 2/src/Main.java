4bfa335f10991653ea2d9aab8130c936247728b2
ximik28 <ximik28@tut.by>
2022-12-15 11:12:18 +0300

    Выполнен Task2

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        int count = 0;
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println("Уволен айтишник с id " + i);
+            count++;
+        }
+        System.out.println("Всего уволено " + count + " сотрудников");
+        System.out.println();
+
+        // решение с помощью while
+        int team = 2;
+        int count2 = 0;
+        while (team <= 100) {
+            System.out.println("Уволен айтишник с id " + team);
+            team += 2;
+            count2 ++;
+        }
+        System.out.println("Всего уволено " + count2 + " сотрудников");
     }
 }
